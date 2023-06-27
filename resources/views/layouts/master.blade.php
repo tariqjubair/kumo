20ac8d46a99f315daffdc89a0d14048fabcf5db9
Tariq <tariq.jubair@gmail.com>
2023-06-28 03:48:25 +0600

    chatbox

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,7 +48,20 @@
 
 <body>
 
-		<!-- ============================================================== -->
+	<!--Start of Tawk.to Script-->
+	<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/649b389594cf5d49dc603068/1h3v5a2no';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+		})();
+	</script>
+
+	<!-- ============================================================== -->
 	<!-- Preloader - style you can find in spinners.css -->
 	<!-- ============================================================== -->
 
